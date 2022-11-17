cd163b0d86f003157892d4b03152d4d95718a4ab
MohamedElsayed2202 <mohamed.elsayed.1.3.1999@gmail.com>
2022-11-17 12:16:06 +0200

    add results work

--- src/Components/TablePoints/TablePoints.js
+++ src/Components/TablePoints/TablePoints.js
@@ -1,4 +1,4 @@
-import  React from 'react';
+import React from 'react';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -10,33 +10,39 @@ import Paper from '@mui/material/Paper';
 
 
 export default function TablePoints(props) {
-  const {rows,handleClickOpen} = props
+  const { rows, handleClickOpen } = props;
   return (
-    <div className='container' >
-     <TableContainer component={Paper} style={{ height: 450, width:'70%' , margin:"auto"}}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Student Name</TableCell>
-            <TableCell align="right">Primary</TableCell>
-            <TableCell align="right">Points</TableCell>
-            <TableCell align="right">Feadback</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody >
-          {rows.map((row) => (
-            <TableRow key={row.studentName} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
-              <TableCell component="th" scope="row" onDoubleClick={()=>{handleClickOpen()}} >
-                {row.studentName}
-              </TableCell>
-              <TableCell align="right">Primary {row.grade}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className='container' style={{height: '80vh'}} >
+      <div className='h-100 w-100 d-flex justify-content-center align-items-center'>
+
+
+        <TableContainer component={Paper} style={{ height: 450, width: '80%'}}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Student Name</TableCell>
+                <TableCell align="right">Primary</TableCell>
+                <TableCell align="right">Subject</TableCell>
+                <TableCell align="right">Points</TableCell>
+                <TableCell align="right">Feadback</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody >
+              {rows.map((row) => (
+                <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
+                  <TableCell component="th" scope="row" onDoubleClick={() => { handleClickOpen(row) }} >
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">Primary {row.grade}</TableCell>
+                  <TableCell align="right">{row.results[0].subject}</TableCell>
+                  <TableCell align="right">{row.results[0].points}</TableCell>
+                  <TableCell align="right">{row.results[0].feadback}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+    </div>
     </div>
   );
 }
